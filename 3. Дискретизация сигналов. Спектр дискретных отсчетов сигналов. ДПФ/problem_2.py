d9094e5a394c1224216560ec65fa95a5b12249cf
100thKing <109743636+100thKing@users.noreply.github.com>
2023-10-04 13:42:05 +0700

    Create problem_2.py

---
+++ 3. Дискретизация сигналов. Спектр дискретных отсчетов сигналов. ДПФ/problem_2.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import kaiserord, lfilter, firwin, freqz
+from scipy import fftpack
+
+Ts = 2e-3
+fs = 1/Ts
+
+t = np.arange(-5, 11)*Ts
+
+for fr in range(50, 300, 50): 
+    plt.figure(fr) 
+    s =  np.cos(fr*t*(2*np.pi))
+    sp = fftpack.fft(s)
+    freqs=np.arange(0,fs,fs/len(s))
+    plt.title(f'Частота {fr} [Hz]')
+    plt.stem(freqs, np.abs(sp))
+    plt.xlabel('Частота в герцах [Hz]')
+    plt.ylabel('Модуль спектра')
+    plt.grid()
